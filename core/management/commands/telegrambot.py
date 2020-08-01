091790c408abe83ec21ce555dce5411636c8e8bc
mh bakhshipour <mh.bakhshipour@yahoo.com>
2020-08-01 16:42:01 +0430

    url

--- core/management/commands/telegrambot.py
+++ core/management/commands/telegrambot.py
@@ -129,7 +129,7 @@ def user_request_service(update, context, user, service, service_sticker_count):
             user_service = UserService.objects.get(user=user, services=service)
 
             new_file = context.bot.get_file(update.message.photo[-1].file_id)
-            i = new_file.download('medias/user_service_image/' + str(update.effective_chat.id) + '.jpg')
+            i = new_file.download('./medias/user_service_image/' + str(update.effective_chat.id) + '.jpg')
 
             if check_similar_user_photo(i, service) is True:
                 user_service = UserService.objects.get(user=user, services=service)
